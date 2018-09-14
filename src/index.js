c54c588523ae4249e0cb7854644407408a177607
Christina-Ilyushonok <iluyshonok.kristina@gmail.com>
2018-09-14 21:26:29 +0300

    Love triangles (task 2 )

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+        let count=0;
+        let x=0;
+        let y=0;
+
+        for (let i = 0; i < preferences.length; i++) {
+            let value = preferences[i];
+            x = preferences[value-1];
+            y = preferences[x-1];
+            if ((i+1) == y && (i+1)!==value) {
+                count++;
+            }
+        }
+        return count/3;
+    };
+
